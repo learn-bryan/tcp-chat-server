6b2458780d1920ceee6cb38100f45945cbb5a0f3
machineblearning <bryan@bryanlearn.com>
2022-08-08 19:00:02 -0400

    changed clientHandler to use separate send() and receive() goroutines

--- server-chat.go
+++ server-chat.go
@@ -17,35 +17,42 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
-// map of client address to connection
-var clients = make(map[string]net.Conn)
+// CLI flags
+var flagIP = flag.String("ip", "127.0.0.1", "Listening IP address")
+var flagPort = flag.String("port", "9000", "Listening port number")
 
-// channel for signaling client disconnect
-var disconnect = make(chan message)
-
-// channel for passing messages
-var messages = make(chan message)
-
-// A message is a representation of the content being
-// shared between clients.
-type message struct {
-	text	string
-	address	string
+// A client is the representation of all the data needed
+// for managing the client and handling messages.
+type client struct {
+	Conn	net.Conn
+	Name	string
+	Message	chan string
 }
 
+// map of client address to client object
+var clients = make(map[string]*client)
+
+// channel for signaling client disconnect
+var disconnect = make(chan string)
+
 func main(){
-	// attempt to listen to a pre-defined local port
-	listen, err := net.Listen("tcp", "localhost:9000")
+	flag.Parse()
+
+	// attempt to listen to provided IP and Port
+	listen, err := net.Listen("tcp", *flagIP+":"+*flagPort)
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("listening on: ", listen.Addr())
 
-	go broadcaster()
+	// server listen/accept loop
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
@@ -58,46 +65,100 @@ func main(){
 // Goroutine that handles client connections.
 // Consumes client input and constructs messages.
 func clientHandler(conn net.Conn){
-	clients[conn.RemoteAddr().String()] = conn
-	messages <- newMessage(" joined", conn)
-	
-	input := bufio.NewScanner(conn)
-	for input.Scan() {
-		messages <- newMessage(": "+input.Text(), conn)
+	log.Printf("clientHandler: connection from: %v", conn.RemoteAddr())
+
+	name, err := readInput(conn, "Enter name: ")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	delete(clients, conn.RemoteAddr().String())
+	// TODO: send welcome message
 
-	disconnect <- newMessage(" disconnected.", conn)
+	// init client object
+	client := &client{
+			Message:	make(chan string),
+			Conn:		conn,
+			Name:		name,
+	}
 
-	conn.Close()
+	// start the send and receive goroutines
+	go client.send()
+	go client.receive()
+
+	clients[conn.RemoteAddr().String()] = client
+	//messages <- newMessage(" joined", conn)
+	
+	//input := bufio.NewScanner(conn)
+	//for input.Scan() {
+	//	messages <- newMessage(": "+input.Text(), conn)
+	//}
+
+	//delete(clients, conn.RemoteAddr().String())
+
+	//disconnect <- newMessage(" disconnected.", conn)
+
+	//conn.Close()
 }
 
-// Goroutine that broadcasts all messages to all clients.
-// Sends client input messages and disconnection messages.
-func broadcaster(){
+func (c *client) receive() {
 	for {
 		select {
-		case msg := <-messages:
-			for _, conn := range clients {
-				if msg.address == conn.RemoteAddr().String() {
-					continue
-				}
-				fmt.Fprintln(conn, msg.text)
-			}
+		case msg := <-c.Message:
+			writeMessage(c.Conn, msg)
 		case msg := <-disconnect:
-			for _, conn := range clients {
-				fmt.Fprintln(conn, msg.text)
-			}
+			writeMessage(c.Conn, msg+" disconnected.")
 		}
 	}
 }
 
-// Constructor for a message struct
-func newMessage(msg string, conn net.Conn) message {
-	addr := conn.RemoteAddr().String()
-	return message {
-		text:		addr + msg + "\n\r",
-		address:	addr,
+func (c *client) send() {
+Loop:
+	for {
+		// read client input
+		msg, err := readInput(c.Conn, "")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// parse commands
+		if msg == "\\quit" {
+			//c.close()
+			log.Printf("%v disconnected.", c.Name)
+			break Loop
+			//break
+		}
+
+		// broadcast message
+		for _, remoteClient := range clients {
+			if c.Conn.RemoteAddr().String() == remoteClient.Conn.RemoteAddr().String() {
+				continue
+			}
+			remoteClient.Message <- c.Name+": "+msg
+		}
+
 	}
+
+	c.close()
+}
+
+// Terminate a client connection
+func (c *client) close() {
+	delete(clients, c.Conn.RemoteAddr().String())
+	disconnect <- c.Name
+	c.Conn.Close()
+}
+
+func readInput(conn net.Conn, qst string) (string, error) {
+	conn.Write([]byte(qst))
+	s, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		log.Printf("readinput: could not read input from stdin: %v from client %v", err, conn.RemoteAddr().String())
+		return "", err
+	}
+	s = strings.Trim(s, "\r\n")
+	return s, nil
+}
+
+func writeMessage(conn net.Conn, msg string) {
+	fmt.Fprintln(conn, msg+"\r")
 }
